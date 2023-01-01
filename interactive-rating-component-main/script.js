f43e5607a145dc0d404049042422bf17b961dd7b
12ayo22 <12ayo22@gmail.com>
2023-01-01 15:11:00 +0900

    [feat] 스크린 리더를 위한 radio 버튼 선택 여부 상태 추가

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -2,13 +2,19 @@
 const rating = document.getElementById('rating');
 const ratingResult = document.getElementById('rating-result');
 const ratingForm = document.getElementById('rating_form');
+const ratingLabels = ratingForm.querySelectorAll('label');
 const submitButton = document.getElementById('submit-button');
 const score = document.getElementById('selected-score');
 
 let currentScore = 0;
 
 ratingForm.addEventListener('change', (event) => {
+  // 웹 접근성 ARIA-CHECK
+  if (currentScore !== 0) {
+    ratingLabels[currentScore - 1].ariaChecked = false;
+  }
   currentScore = parseInt(event.target.value, 10);
+  ratingLabels[currentScore - 1].ariaChecked = true;
   score.textContent = currentScore;
 });
 
